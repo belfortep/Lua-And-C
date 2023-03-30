441f7da58dd85b660d4f6ac1d81e891fc862f4b5
belfortep <pbelforte2003@gmail.com>
2023-03-30 09:06:08 -0300

    creating tables in lua from c

--- lua_parser.c
+++ lua_parser.c
@@ -2,22 +2,43 @@
 #include "lua.h"
 #include "lualib.h"
 
-//cc lua_parser.c -o lua_parser -llua -L../lua -I../lua -lm -ldl 
+//gcc lua_parser.c -o lua_parser -llua -L../lua -I../lua -lm -ldl 
 
-const void *execute_lua_code(char *lua_code)
+
+void setfield(lua_State *L,const char *index, int value, char *table_name)
+{
+        lua_newtable(L);
+        lua_pushstring(L, index);
+        lua_pushnumber(L, value);
+        lua_settable(L, -3);
+        lua_setglobal(L, table_name);
+}
+
+
+// idea interesante, yo te ejecuto el codigo de lua
+// y tambien puedo hacer que me pases un json_t y convertirtelo a una table de lua
+// ta bien
+lua_State *execute_lua_code(char *lua_code)
 {
         lua_State *L = luaL_newstate();
         luaL_openlibs(L);
-        //luaL_dostring(L, lua_code);
-        luaL_dofile(L, "script.lua");
-        return lua_topointer(L, -1);;
+        luaL_dofile(L ,lua_code);
+
+        return L;
 }
 
 int main()
 {
-        const char *sarasa = execute_lua_code("return 'hello'");
-        //printf("sarasa es: %s", sarasa);
+        char *table_name = "nombre_tabla";
+        lua_State *L = execute_lua_code("script.lua");
+        setfield(L, "key", 25, table_name);
+        lua_getglobal(L, table_name);
+        lua_pushstring(L, "key");
+        lua_gettable(L, -2);
+        int result = (int)lua_tonumber(L, -1);
+        lua_pop(L, 1);
 
+        printf("result := %i \n", result);
 
         return 0;
 }
